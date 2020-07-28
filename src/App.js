3e29dff583bc1ba5c69421564a95ea9d63eba831
Urvashi <uk00477186@techmahindra.com>
2020-07-28 13:03:20 +0530

    New Branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import LoginComp from './Components/Auth/LoginComp'
 
 function App() {
   return (
     <div className="App">
      <h1>Jobzilla</h1>
+     <LoginComp></LoginComp>
     </div>
   );
 }
