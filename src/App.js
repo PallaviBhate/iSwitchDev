f99abf842671f07ad4ad1205e3c4eba306f9972e
Sakshi_D <sd00615274@techmahindra.com>
2020-08-04 10:31:18 +0530

    Added Dashboard.jsx and updated App.js by importing Dashboard.jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-<<<<<<< HEAD
 import UploadProfile from './Components/ProviderComp/UploadProfile';
+import Dashboard from './Components/ProviderComp/Dashboard';
 
 
-=======
-import LoginComp from './Components/Auth/LoginComp'
->>>>>>> 6f3bbfd395842de8183297f064293d259cf0c962
 
 function App() {
   return (
     <div className="App">
      <h1>Jobzilla</h1>
-<<<<<<< HEAD
      <UploadProfile/>
-=======
-     <LoginComp></LoginComp>
->>>>>>> 6f3bbfd395842de8183297f064293d259cf0c962
+     <Dashboard/>
     </div>
   );
 }
