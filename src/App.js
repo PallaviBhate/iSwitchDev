cd178ffd30dc37fee5b2765ba68e0ca8f1b64ec7
Prateek <pr00613027@techmahindra.com>
2020-08-06 11:20:45 +0530

    1.Updated UploadProfile.jsx with the code of Download sample file option.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './Assets/css/Style.css'
 import RouterSettings from './Routing/RouterSettings';
 import ProviderDashboard from './Components/ProviderComp/ProviderDashboard'
+import UploadProfile from './Components/ProviderComp/UploadProfile';
 
 function App() {
   return (
     <div className="App">
      <RouterSettings></RouterSettings>
     {/* <ProviderDashboard/> */}
+    {/* <UploadProfile/> */}
     </div>
   );
 }
