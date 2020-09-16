971204c006299301f4053917868376bc821d9eb0
Prashant Darshan <pd00558235@techmahindra.com>
2020-09-16 16:38:28 +0530

    commit for image loading

--- src/Components/CommonComp/Dashboard/OverviewCard.jsx
+++ src/Components/CommonComp/Dashboard/OverviewCard.jsx
@@ -11,14 +11,14 @@ export default class OverviewCard extends Component {
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
                         <section className="uploadedProfile_section">
                             <div className="media">
-                                <img className="mr-3" src="images/dashboard-assets/uploaded-profile.svg" alt="Profile Uploaded image" />
+                                <img className="mr-3" src="/images/dashboard-assets/uploaded-profile.svg" alt="Profile Uploaded image" />
                                 <div className="media-body">
                                     <h5 className="mt-0 overview_numbers">
                                         20345
 							{/* <CountUp start={0} end={confirmed.value} duration={2.75} separator="," /> */}
                                     </h5>
 							Number of profile uploaded.
-							<img src="images/dashboard-assets/more_ico.png" width="10" height="10" className="more_ico" />
+							<img src="/images/dashboard-assets/more_ico.png" width="10" height="10" className="more_ico" />
                                 </div>
                             </div>
                         </section>
@@ -26,14 +26,14 @@ export default class OverviewCard extends Component {
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
                         <section className="activeProfile_section">
                             <div className="media">
-                                <img className="mr-3" src="images/dashboard-assets/active-profile.svg" alt="Active Profile image" />
+                                <img className="mr-3" src="/images/dashboard-assets/active-profile.svg" alt="Active Profile image" />
                                 <div className="media-body">
                                     <h5 className="mt-0 overview_numbers">
                                         15145
 					        {/* <CountUp start={0} end={recovered.value} duration={2.75} separator="," /> */}
                                     </h5>
 							Number of active profiles.
-							<img src="images/dashboard-assets/more_ico.png" width="10" height="10" className="more_ico" />
+							<img src="/images/dashboard-assets/more_ico.png" width="10" height="10" className="more_ico" />
                                 </div>
                             </div>
                         </section>
@@ -41,14 +41,14 @@ export default class OverviewCard extends Component {
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
                         <section className="hiredProfile_section">
                             <div className="media">
-                                <img className="mr-3 mt-2" src="images/dashboard-assets/offer-ico.svg" alt="Hired Profile image" />
+                                <img className="mr-3 mt-2" src="/images/dashboard-assets/offer-ico.svg" alt="Hired Profile image" />
                                 <div className="media-body">
                                     <h5 className="mt-0 overview_numbers">
                                         1345
 						    {/* <CountUp start={0} end={deaths.value} duration={2.75} separator="," /> */}
                                     </h5>
 							Number of hired profile.
-							<img src="images/dashboard-assets/more_ico.png" width="10" height="10" className="more_ico" />
+							<img src="/images/dashboard-assets/more_ico.png" width="10" height="10" className="more_ico" />
                                 </div>
                             </div>
                         </section>
--- src/Components/CommonComp/HeaderAll.jsx
+++ src/Components/CommonComp/HeaderAll.jsx
@@ -39,17 +39,18 @@ class HeaderAll extends Component{
             {/* <div className="float-left logo_container col-xl-2">
                 <img src="images/Dashboard-assets/logo-white.png" className="logo"/>
             </div> */}
-            <div className="float-left mt-2 d-inline-flex">
-          <div className="mx-3 sub-title1 d-flex align-items-center">JOB : </div>
-          <div className="btn-group btn-group-toggle" data-toggle="buttons">
-            <label className="btn btn-toggler active">
-              <input type="radio" name="options" id="option1" autoComplete="off" checked /> Provider
-            </label>
-            <label className="btn btn-toggler">
-              <input type="radio" name="options" id="option2" autoComplete="off" /> Recruiter
-            </label>
-          </div>
-        </div>
+            {(!this.props.isCandidate) ?
+              <div className="float-left mt-2 d-inline-flex">
+                <div className="mx-3 sub-title1 d-flex align-items-center">JOB : </div>
+                <div className="btn-group btn-group-toggle" data-toggle="buttons">
+                  <label className="btn btn-toggler active">
+                    <input type="radio" name="options" id="option1" autoComplete="off" checked /> Provider
+                </label>
+                  <label className="btn btn-toggler">
+                    <input type="radio" name="options" id="option2" autoComplete="off" /> Recruiter
+                </label>
+                </div>
+              </div> : null}
                     <ul className="nav mt-2">
                       <li className="nav-item">
                         <a className="nav-link" href="#" data-toggle="dropdown">
@@ -80,7 +81,7 @@ class HeaderAll extends Component{
                         </ul>
                       </li>
                       <li>          
-                        <img className="rounded-circle profile-icon mr-3" src="images/Dashboard-assets/user-f.jpg" width="35" height="35"/>
+                        <img className="rounded-circle profile-icon mr-3" src="/images/Dashboard-assets/user-f.jpg" width="35" height="35"/>
                       </li>
                     </ul>
                    </header>
