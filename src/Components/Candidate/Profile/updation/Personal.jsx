ac1307dc3c2c8fd97f4f9306ad6b86a8cb101641
“rishi <rp00575457@techmahindra.com>
2020-09-28 12:57:29 +0530

    persional info update api work

--- src/Components/Candidate/Profile/updation/Personal.jsx
+++ src/Components/Candidate/Profile/updation/Personal.jsx
@@ -2,12 +2,26 @@ import React, { useState } from 'react';
 import DatePicker from 'react-date-picker';
 import ReactTags from 'react-tag-autocomplete'
 import { COUNTRY_LIST } from '../../../../Utils/AppConst';
+import ApiServicesOrgCandidate from '../../../../Services/ApiServicesOrgCandidate';
 
 
 
 const Personal = () => {
+  const [inputData, setFormInputData] = React.useState({
+    "dob": new Date(),
+    "gender": "",
+    "passportId": "",
+    "address": "",
+    "maritalStatus": "",
+    "pincode": "",
+    "city": "",
+    "state": "",
+    "country": "",
+    "workPermit": ""
+  })
   const [isGender, setGender] = useState('male');
   const [tags, setTags] = useState([]);
+  const [workPermit, setWorkPermit] = useState([]);
   const [startDate, setStartDate] = useState(new Date());
 
   const onValueChange = (event) => {
@@ -15,13 +29,45 @@ const Personal = () => {
   }
 
   const onAddition = (tag) => {
-    const tagsCnt = [].concat(tags, tag)
-    setTags(tagsCnt)
+    const tagsCnt = [].concat(tags, tag);
+    setWorkPermit(oldArray => [...oldArray, tag.name]);
+    setTags(tagsCnt);
   }
   const onDelete = (i) => {
     const tagsCnt = tags.slice(0)
-    tagsCnt.splice(i, 1)
+    tagsCnt.splice(i, 1);
+    setWorkPermit(Array.prototype.map.call(tagsCnt, s => s.name));
     setTags(tagsCnt)
+  }
+
+  const handleSubmit = (e) => {
+    const candidateId = localStorage.getItem('candidateId')
+    let data = {
+      "dob": startDate,
+      "gender": isGender,
+      "passportId": inputData.passportId,
+      "address": inputData.address,
+      "maritalStatus": inputData.maritalStatus,
+      "pincode": inputData.pincode,
+      "city": inputData.city,
+      "state": inputData.state,
+      "country": inputData.country,
+      "workPermit":  workPermit.join(),
+      "candidateId": candidateId
+    }
+    console.log(data)
+    ApiServicesOrgCandidate.updateCareerInfo(data);
+    e.preventDefault();
+  }
+
+  const handleFormInputData = (e) => {
+    console.log(e)
+    return (
+      setFormInputData({
+        ...inputData,
+        [e.target.name]: e.target.value
+      })
+    )
   }
 
   return (
@@ -32,7 +78,10 @@ const Personal = () => {
             <label className="modal-label" htmlFor="University">Date of Birth</label>
             <div>
               <DatePicker
-                value={startDate}
+                // value={startDate}
+                value={inputData.dob}
+                name={"dob"}
+                //onChange={(e) => handleFormInputData(e)}
                 onChange={date => setStartDate(date)}
                 calendarIcon={<img src="../images/profile/calendar.png" style={{ width: '16px' }} />}
                 clearIcon={null}
@@ -81,19 +130,47 @@ const Personal = () => {
           </div>
           <div className="form-group">
             <label className="modal-label" htmlFor="University">Passport ID</label>
-            <input class="form-control" type="text" placeholder="LKJ1234" />
+            <input class="form-control"
+              type="text"
+              placeholder="LKJ1234"
+              id="passportId"
+              required
+              name="passportId"
+              value={inputData.passportId}
+              onChange={(e) => handleFormInputData(e)}
+            />
           </div>
           <div className="form-group">
             <label className="modal-label" htmlFor="University">Address</label>
-            <input class="form-control" type="text" placeholder="85 Flat XYZ Building" />
+            <input class="form-control" type="text"
+              placeholder="85 Flat XYZ Building"
+              id="address"
+              required
+              name="address"
+              value={inputData.address}
+              onChange={(e) => handleFormInputData(e)}
+            />
           </div>
           <div class="form-group">
             <div class="form-row">
               <div className="col mr-3">
-                <input class="form-control" type="text" placeholder="Pin Code" />
+                <input class="form-control"
+                  type="text" placeholder="Pin Code"
+                  value={inputData.pincode}
+                  id="pincode"
+                  required
+                  name="pincode"
+                  onChange={(e) => handleFormInputData(e)}
+                />
               </div>
               <div className="col ml-3">
-                <select id="University" className="form-control">
+                <select className="form-control"
+                  value={inputData.city}
+                  id="city"
+                  required
+                  name="city"
+                  onChange={(e) => handleFormInputData(e)}
+                >
                   <option>Mumbai</option>
                   <option>Delhi</option>
                   <option>Patna</option>
@@ -105,7 +182,13 @@ const Personal = () => {
           <div class="form-group">
             <div class="form-row">
               <div className="col mr-3">
-                <select id="University" className="form-control">
+                <select className="form-control"
+                  value={inputData.state}
+                  id="state"
+                  required
+                  name="state"
+                  onChange={(e) => handleFormInputData(e)}
+                >
                   <option>Maharashtra</option>
                   <option>Uttar Pradesh</option>
                   <option>Delhi</option>
@@ -113,7 +196,13 @@ const Personal = () => {
                 </select>
               </div>
               <div className="col ml-3">
-                <select id="University" className="form-control">
+                <select className="form-control"
+                  value={inputData.country}
+                  id="country"
+                  required
+                  name="country"
+                  onChange={(e) => handleFormInputData(e)}
+                >
                   <option>India</option>
                   <option>America</option>
                   <option>Russia</option>
@@ -123,8 +212,14 @@ const Personal = () => {
             </div>
           </div>
           <div className="form-group">
-            <label className="modal-label" htmlFor="University">Marital Status</label>
-            <select id="University" className="form-control">
+            <label className="modal-label" htmlFor="maritalStatus">Marital Status</label>
+            <select className="form-control"
+              value={inputData.maritalStatus}
+              id="maritalStatus"
+              required
+              name="maritalStatus"
+              onChange={(e) => handleFormInputData(e)}
+            >
               <option>Single/unmarried</option>
               <option>Married</option>
               <option>Widowed</option>
@@ -143,7 +238,7 @@ const Personal = () => {
               onAddition={onAddition} />
           </div>
         </div>
-        <button class="btn lightBlue float-right px-5">Save</button>
+        <button class="btn lightBlue float-right px-5" onClick={handleSubmit}>Save</button>
       </form>
     </>
   );
