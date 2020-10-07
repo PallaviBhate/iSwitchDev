a0598654373ab5ac521c09be1ab01957ccf574c5
pallavi.rbhate <pallavi.rbhate@gmail.com>
2020-10-07 16:56:26 +0530

    JWT Token Implemented

---
+++ src/Components/RecruiterComp/CloseJobs.jsx
@@ -0,0 +1,14 @@
+import React,{Component, Fragment} from 'react'
+import LeftNavProvider from '../CommonComp/LeftNavProvider'
+import HeaderAll from '../CommonComp/HeaderAll'
+import Footer from '../CommonComp/Footer'
+import { Toast } from 'primereact/toast'
+
+class CloseJobs extends Component{
+    render(){
+        return(
+            <div>This is Closed Job feature</div>
+            )
+        }
+    }
+    export default CloseJobs                                                               
--- src/Config.jsx
+++ src/Config.jsx
@@ -1,5 +1,6 @@
 
 //Common Header to use in API call
-export const ApiHeader = {headers: { 'Content-Type': 'application/json',}};
+export const ApiHeader = {headers: { 'Content-Type': 'application/json'}}
+
 // Base URL to use
 export const ApiBaseUrl = "https://techm-jobzilla.herokuapp.com/jobs";
